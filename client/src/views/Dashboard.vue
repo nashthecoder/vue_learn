34fa4b16de96badcb9ffdd86df69fd7ccd189cc3
NashTheCoder <naijeria@gmail.com>
2020-12-25 01:32:54 +0300

    formatted sidebar menu

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,38 +1,33 @@
 <template>
-  <div id="sidebar">
-    <ul>
-      <li><a href="/line">Line</a></li>
-      <li><a href="/bar">Bar</a></li>
-      <li><a href="/doughnut">Doughnut</a></li>
-      <li><a href="/pie">Pie</a></li>
-      <li><a href="/radar">Radar</a></li>
-      <li><a href="/polar-area">Polar Area</a></li>
-      <li><a href="/bubble">Bubble</a></li>
-      <li><a href="/scatter">Scatter</a></li>
-    </ul>
-    <div id="sidebar-btn" v-on:click="exibir()">
-      <span></span>
-      <span></span>
-      <span></span>
+  <div class="dashboard">
+    <Sidebar />
+    <div class="content">
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
-import $ from "jquery";
+import Sidebar from "../components/Sidebar";
 export default {
-  name: "dashboard",
-  props: ["mostrar"],
-  methods: {
-    exibir: () => {
-      $("#sidebar").toggleClass("visible");
-    }
+  name: "home",
+  components: {
+    Sidebar
   }
 };
 </script>
 
-<style scoped>
-#sidebar {
-  display: flex;
+<style>
+.dashboard {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  background-color: #f6819d;
+  height: 100vh;
+  width: 100vw;
+}
+.content {
+  background-color: white;
+  border-radius: 10px;
+  margin: 6px 6px 6px 0px;
 }
 </style>
